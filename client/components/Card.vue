85d2316ae60ef322741b86afe350ad7f8e76d9c8
Gauthier LO <lo.gauthier.glo@gmail.com>
2024-05-25 17:48:53 +0200

    test prod tag bug

--- client/components/Card.vue
+++ client/components/Card.vue
@@ -8,6 +8,7 @@
         </div>
         <div class="flex flex-wrap gap-1">
           <tag v-for="tag in props.tags" :name="tag.name" :component="tag.icon" :color="tag.color"></tag>
+          <span v-for="tag in tags">{{ tag.name }}</span>
         </div>
       </header>
       <div class="text-gray-500 dark:text-gray-500 mb-2">
