d8816d75cbfc945fa772b9a0e02afb07fd28718a
Gauthier LO <lo.gauthier.glo@gmail.com>
2024-05-26 10:56:25 +0200

    sitemap

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/sitemap"],
   css: ['~/assets/main.css'],
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
     configPath: '~/tailwind.config.ts',
-  }
-
+  },
+  site: {
+    url: 'https://terrainconfus.fr',
+  },
 })
