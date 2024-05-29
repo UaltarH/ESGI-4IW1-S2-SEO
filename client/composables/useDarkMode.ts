dabc20f6f4e640e26a3d24031536d3f76a221056
Gauthier LO <lo.gauthier.glo@gmail.com>
2024-05-29 15:26:03 +0200

    (WIP) toggle change to button

--- client/composables/useDarkMode.ts
+++ client/composables/useDarkMode.ts
@@ -1,32 +1,36 @@
 export const useDarkMode = () => {
+    // const toggleDarkMode = (e: Event) => {
+    //     console.log(e)
+    //     if(e.target === null) {
+    //         return;
+    //     }
+    //     if(!(e.target instanceof HTMLInputElement))
+    //         return;
+    //     applyDarkMode(e.target.checked);
+    //     localStorage.setItem("darkMode", e.target.checked.toString());
+    // };
     const toggleDarkMode = (e: Event) => {
+        console.log(e)
         if(e.target === null) {
             return;
         }
-        if(!(e.target instanceof HTMLInputElement))
+        if(!(e.target instanceof HTMLElement))
             return;
-        applyDarkMode(e.target.checked);
-        localStorage.setItem("darkMode", e.target.checked.toString());
-    }
-    const applyDarkMode = ((checked: boolean) => {
+        applyDarkMode();
+        // localStorage.setItem("darkMode", e.target.checked.toString());
+    };
+    const applyDarkMode = (() => {
         const html = document.getElementsByTagName("html")[0];
         const toggle = document.getElementById("dark-mode-toggle");
         if(toggle === null) {
             return;
         }
-        else {
-            (toggle as HTMLInputElement).checked = checked;
-        }
-        if(checked) {
-            html.classList.add("dark");
-        } else {
-            html.classList.remove("dark");
-        }
+        html.classList.toggle("dark");
     });
     const loadDarkModePreference = (() => {
         const darkMode = localStorage.getItem("darkMode");
         const isDarkMode = darkMode === "true";
         applyDarkMode(isDarkMode);
     });
-    return {toggleDarkMode, loadDarkModePreference}
-}
+    return {toggleDarkMode, loadDarkModePreference};
+};
